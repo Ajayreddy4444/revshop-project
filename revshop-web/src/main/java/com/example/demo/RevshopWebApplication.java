60e7552fca0c0e76f656404571562dca23c14268
Ajayreddy4444 <ajayreddygangasani16@gmail.com>
2026-02-19 19:19:31 +0530

    Initial project structure

---
+++ revshop-web/src/main/java/com/example/demo/RevshopWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevshopWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevshopWebApplication.class, args);
+	}
+
+}
