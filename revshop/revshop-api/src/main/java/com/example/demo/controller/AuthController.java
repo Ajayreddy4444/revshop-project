5b033d9ba8014b7bd45a7c3c702e45fe4de361c9
Ajayreddy4444 <ajayreddygangasani16@gmail.com>
2026-02-23 19:56:41 +0530

    Implemented forgot password feature backend

--- revshop/revshop-api/src/main/java/com/example/demo/controller/AuthController.java
+++ revshop/revshop-api/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.ForgotPasswordRequest;
 import com.example.demo.dto.LoginRequest;
 import com.example.demo.dto.RegisterRequest;
+import com.example.demo.dto.ResetPasswordRequest;
 import com.example.demo.service.AuthService;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,5 +32,21 @@ public class AuthController {
     @PostMapping("/login")
     public AuthResponse login(@RequestBody LoginRequest request){
         return authService.login(request);
+    }
+    
+    @PostMapping("/forgot-password")
+    public String forgotPassword(@RequestBody ForgotPasswordRequest request) {
+        authService.forgotPassword(request.getEmail());
+        return "OTP sent (check your mail)";
+    }
+
+    @PostMapping("/reset-password")
+    public String resetPassword(@RequestBody ResetPasswordRequest request) {
+        authService.resetPassword(
+                request.getEmail(),
+                request.getOtp(),
+                request.getNewPassword()
+        );
+        return "Password updated successfully";
     }
 }
