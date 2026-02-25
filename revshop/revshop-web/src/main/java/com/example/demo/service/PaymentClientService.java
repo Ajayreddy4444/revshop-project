9ff37059000e982eeae68ee0b1bbfad600721fd9
shivanidaliparthi <shivanidaliparthi@gmail.com>
2026-02-26 01:27:56 +0530

    Remove payment web module from order branch

--- revshop/revshop-web/src/main/java/com/example/demo/service/PaymentClientService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.dto.PaymentRequestDTO;
-
-public interface PaymentClientService {
-
-    void processPayment(PaymentRequestDTO request);
-}
